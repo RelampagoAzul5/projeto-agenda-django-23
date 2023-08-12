7261bd70a95f880dcc1ace5eccb1267c9e02fde7
RelampagoAzul5 <giovannihpf@gmail.com>
2023-08-11 23:07:02 -0300

    criando a area do contato unico

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
@@ -10,6 +10,16 @@ from django.utils import timezone
 # picture (imagem)
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -17,6 +27,16 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL, blank=True,
+        null=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL, blank=True,
+        null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+# type: ignore
+# flake8: noqa
 """
 Django settings for project project.
 
@@ -122,8 +124,15 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from project.local_settings import *
+except ImportError:
+    raise ImportError("Create 'local_settings.py'")
