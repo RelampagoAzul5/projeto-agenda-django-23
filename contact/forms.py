0a6765e039811c361ec9b0fe73226ac38974ab14
RelampagoAzul5 <giovannihpf@gmail.com>
2023-08-20 17:45:00 -0300

    Criando a area de atualização de dados do usuário

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from . import models
 from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
@@ -81,3 +82,86 @@ class RegisterForm(UserCreationForm):
                     'Ja existe um usuário com esse email',
                     code='invalid')
             )
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.',
+        error_messages={
+            'min_length': 'Please, add more than 2 letters.'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.'
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+
+    password2 = forms.CharField(
+        label="Password 2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text='Use the same password as before.',
+        required=False,
+    )
+    
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username',)
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+ 
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('Senhas incompativeis')
+                )
+        return super().clean()
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError(
+                        'Ja existe um usuário com esse email',
+                        code='invalid')
+                )
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+                
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
 
 
@@ -20,6 +20,31 @@ def register(request):
             'form': form
         }
     )
+
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST,instance=request.user)
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+    form.save()
+    return redirect('contact:user_update')
+
 def login_view(request):
     form = AuthenticationForm(request)
     if request.method == 'POST':
@@ -39,6 +64,8 @@ def login_view(request):
             'form': form
         }
     )
+
+
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
