a941e2e8cca7ac0d905b2260661a6ca56e91ab64
RelampagoAzul5 <giovannihpf@gmail.com>
2023-08-20 14:16:57 -0300

    Criando a area de login do usuário

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from . import models
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -48,3 +50,35 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField(
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError(
+                    'Ja existe um usuário com esse email',
+                    code='invalid')
+            )
--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from .contact_views import *
 from .contact_forms import *
+from .user_forms import *
